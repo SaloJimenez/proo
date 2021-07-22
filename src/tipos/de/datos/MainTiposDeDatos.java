5f7dd81af13633071ffd45feadd235f2c6270411
USER <USER@DESKTOP-O8V4P81>
2021-07-21 21:07:48 -0500

    Taller

---
+++ src/tipos/de/datos/MainTiposDeDatos.java
@@ -0,0 +1,16 @@
+package tipos.de.datos;
+
+public class MainTiposDeDatos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		short cantidadEstudiantesUCE = 1000;
+		byte entradaCircuito = 01;
+		int cantidadCiudadanos = 100000;
+		long cantidadMundo = 1000000000;
+		float sueldo = 1200.56f;
+		double saldoBanco = 10000000.67;
+		char opcionMenu = 'c';
+		boolean casado = true;
+	}
+}
