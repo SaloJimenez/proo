1fc72108165000f67bd8d9c9d8884e6545f6b8f1
USER <USER@DESKTOP-O8V4P81>
2021-08-03 19:39:40 -0500

    Ejercicio Vector 1

---
+++ src/ejercicios/varios/vectores/matrices/MainVector1.java
@@ -0,0 +1,23 @@
+package ejercicios.varios.vectores.matrices;
+
+import java.util.Scanner;
+
+public class MainVector1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Declarar un vector X[3] de tipo char y cosntruya un programa que pida al usuario que ingrese valores para cada posición y finalmente que se imprima todos los valores ingresados.
+		     char X[]  = new char[3];
+		     for (int i = 0; i < 3; i++) {
+		    	 System.out.println("Ingrese el valor de la posición: " + i);
+		    	
+		    	 Scanner lector = new Scanner (System.in);
+		    	 char n = lector.next().charAt(0);
+		    	 X[i]=n;
+		    	 
+		     }
+		
+	}
+
+}
