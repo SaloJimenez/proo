8c5779090f89ac2683cc6122f5ca313f478cc0d0
USER <USER@DESKTOP-O8V4P81>
2021-08-11 20:07:42 -0500

    Matriculación

---
+++ src/ejercicio/metodos/Matriculacion.java
@@ -0,0 +1,20 @@
+package ejercicio.metodos;
+
+public class Matriculacion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Debería validar que pasó el alumno
+		//Debería validad que no hay cruces de materias
+		//Que existan profesores
+		//Validaciones en general de la matriculación
+		//Si todo está bien (if)
+		//Debería mandar a imprmir
+		
+		Impresora impresion1 = new Impresora(); //instanciar la clase
+		impresion1.imprmir("Matriculación exitosa ·123456", 12);
+
+	}
+
+}
