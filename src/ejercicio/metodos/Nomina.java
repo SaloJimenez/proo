b00bbee3dde3d416cc39bc2b901a1d929d397214
USER <USER@DESKTOP-O8V4P81>
2021-08-11 20:08:04 -0500

    Nómina

---
+++ src/ejercicio/metodos/Nomina.java
@@ -0,0 +1,16 @@
+package ejercicio.metodos;
+
+public class Nomina {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Consultar odcente
+		//Ver que su contrato esté activo
+		
+		Impresora impresion1 = new Impresora(); //instanciar la clase
+		impresion1.imprmir("Pepito Pérez: $1.200", 8);
+
+	}
+
+}
