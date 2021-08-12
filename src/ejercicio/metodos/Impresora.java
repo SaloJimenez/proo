04e06c2ecd6e4075199895055e34982246cfb8be
USER <USER@DESKTOP-O8V4P81>
2021-08-11 20:07:28 -0500

    Impresora

---
+++ src/ejercicio/metodos/Impresora.java
@@ -0,0 +1,32 @@
+//1.Declaración del paquete
+package ejercicio.metodos;
+
+//2.Declaración de importaciones
+
+//3.Comentarios
+//Clase que se encarga de realizar una impresión en formato A4
+
+//4.Declaración de la clase
+public class Impresora {
+	
+	//5.Constantes
+	//6.Variables
+	public String color;
+	
+	//7.Constructor
+	public Impresora() {
+		
+	} 
+	
+	//8.Métodos
+	public void imprmir(String datosAImprimir, int letra) {
+		System.out.println(" Logo UCE ");
+		System.out.println(" ******** ");
+		System.out.println(datosAImprimir);
+		System.out.println(" ******** ");
+		System.out.println(" Pie de firma ");
+		
+		
+	}
+
+}
