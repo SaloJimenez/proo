cfad98d26edb3dc45001da075aa1c222d9490748
USER <USER@DESKTOP-O8V4P81>
2021-08-11 20:07:53 -0500

    Notas

---
+++ src/ejercicio/metodos/Notas.java
@@ -0,0 +1,17 @@
+package ejercicio.metodos;
+
+public class Notas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Verificar si soy estudiante
+		//Consulta notas
+		//Si existe notas
+		
+		Impresora impresion1 = new Impresora(); //instanciar la clase
+		impresion1.imprmir("Programación 1: 20 - Investigación 17", 8);
+
+	}
+
+}
